a091a81fbece53fc72e7dd034006efd1a9a656bb
Marcos Lopez <7751091+mdl0100@users.noreply.github.com>
2023-06-13 19:14:29 -0500

    graphbviz issue

--- Assignments/A05/csv2json.py
+++ Assignments/A05/csv2json.py
@@ -1,48 +1,70 @@
+"""
+Marcos Lopez
+csv2json.py
+
+This program serves to take the dwarf_family_tree.csv in ./familyData/ and convert it to a useable JSON file for the family tree assignment. 
+The names of the dwarfs are changed in the process using the mockaroo_family_tree_2.csv file as the new names
+The irrelevant keys from dwarf_family_tree.csv are stripped out of the dictionary
+The improved dictionary is then exported as a json file using dumps, meaning that it'll be a string
+
+
+A few notes about this file:
+    - dwarf_family_tree.csv is read in and parsed into a dictionary
+    - mockaroo_family_tree_2.csv is read in and parsed into a dictionary
+        - 3 name stacks are created to hold last name, male first names, and female first names. 
+    - the family dictionary is updated with the new names using the stacks
+
+"""
+
 import csv
 import json
 from rich import print
-import random
 
+# Open dwarf_family_tree.csv and convert its contents to a Python dictionary
 data = {}
 with open ('./familyData/dwarf_family_tree.csv') as f:
     csvRead = csv.DictReader(f)
     for rows in csvRead:
-        key = rows["#pid"]
+        key = int(rows["#pid"])
         data[key] = rows
 
-clans = ['Stonehammer', 'Ironbeard', 'Deepforge', 'Thunderpeak', 'Silveraxe', 'Frostbeard', 'Boulderfist', 'Fireheart', 'Goldhammer', 'Swiftsteel']
-fnameQMale = []
-fnameQFemale = []
-lnameQ = []
+# Create 3 stacks for Male first names, Female First Names, and Last Names to replace the Dwarf names in data
+fnameMaleStack = []
+fnameFemaleStack = []
+lnameStack = []
+
+# Open the mockaroo data and extract male/female first names and last names
 namingDict = {}
 with open('./familyData/mockaroo_family_tree_2.csv') as f:
     csvRead = csv.DictReader(f)
     for rows in csvRead:
         key = rows["id"]
         namingDict[key] = rows
+    # push names to the the correct stacks based on gender
     for id in namingDict:
         person = namingDict[id]
         if person['gender'] == "Male":
-            fnameQMale.append(person['first_name'])
+            fnameMaleStack.append(person['first_name'])
         else:
-            fnameQFemale.append(person['first_name'])
-        lnameQ.append(person['last_name'])
+            fnameFemaleStack.append(person['first_name'])
+    #last names of every person to added to the last name stack
+        lnameStack.append(person['last_name'])
 
-finalDict = {}
+# Add in new first and last names for each person using the stacks
 for id in data:
     person = data[id]
-    person["fname"] = fnameQMale.pop() if person['gender'] == 'M' else fnameQFemale.pop()
-    person["lname"] = lnameQ.pop()
-    person.pop('name', None)
-    person.pop('generation', None)
-    person.pop('myear', None)
-    person.pop('mage', None)
-    person.pop('ptype', None)
-    person.pop('parentNodeId', None)
-    person.pop('#pid', None)
-    
+    person["fname"] = fnameMaleStack.pop() if person['gender'] == 'M' else fnameFemaleStack.pop()
+    person["lname"] = lnameStack.pop()
+    #Delete each unncessary attribute from the dictionary
+    person.pop('name', None)        #dwarf's name deleted
+    person.pop('generation', None)  #generate number deleted
+    person.pop('myear', None)       #marriage year deleted
+    person.pop('mage', None)        #marriage age deleted
+    person.pop('ptype', None)       #personality type deleted
+    person.pop('parentNodeId', None)#parentNodeId deleted
+    person.pop('#pid', None)        #pid deleted, since it's also serving as the key for the dictionary
 
 
-with open('./familyData/dwarf_family_tree.json', 'w', encoding='utf-8') as f:
+# export data as a json string dictionary using dumps
+with open('./dwarf_family_tree.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(data, indent=4))
-
--- Assignments/A05/family.py
+++ Assignments/A05/family.py
@@ -1,5 +1,40 @@
+"""
+Marcos Lopez 
+Assignment - A05 - Family Tree
+
+This program takes in the dwarf_family_tree.json file and creates a .dot file and a family tree visualization of the 
+"""
+
 import json
-import csv
-from rich import print_json
+import graphviz
+from rich import print
 
 
+with open('./dwarf_family_tree.json', 'r') as f:
+    family = json.load(f)
+
+
+family_int = {}
+for key, person in family.items():
+    family_int[int(key)] = person
+    person['clan'] = int(person['clan'])
+    person['parentId1'] = int(person['parentId1']) if person['parentId1'] else None
+    person['parentId2'] = int(person['parentId2']) if person['parentId2'] else None
+    person['spouseId'] = int(person['spouseId']) if person['spouseId'] != '' else -1
+    person['gender'] = 0 if person['gender'] == "M"  else 1
+
+print(family_int)
+
+clans = ['Stonehammer', 'Ironbeard', 'Deepforge', 'Thunderpeak', 'Silveraxe', 'Frostbeard', 'Boulderfist', 'Fireheart', 'Goldhammer', 'Swiftsteel']
+shapes = ['oval', 'egg']
+colors = ['0000ff', '00ffff', '00ff00', 'ff00ff', 'ff9900', 'ffff00', 'ffffff', '660099', '996600', '336633']
+
+
+dot = graphviz.Digraph(comment="The Dwarven Realm")
+for key, person in family_int.items():
+    fname = person['fname']
+    lname = person['lname']
+    gender = person['gender']
+    dot.node(str(key), f'{fname} {lname}')
+
+print(dot.source)
